5bb9bf0c2c1074974ef37d55681ef38a1811a02c
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-11 23:04:07 +0500

    Получение карты. подсчет очков. Показ карт

--- player.rb
+++ player.rb
@@ -1,6 +1,37 @@
 class Player
   attr_accessor :name
-  def initialize(name)
+  attr_reader :score
+
+  def initialize(name=nil, bank)
     @name = name
+    @bank = bank
+    @cards = []
+    @score = 0
+  end
+
+  def <<(card)
+    @cards << card
+    if card.ace?
+      if @score<=10
+        @score += card.nominal
+      else
+        @score +=1
+      end
+    else
+      @score += card.nominal
+    end
+  end
+
+  def show_cards(visible)
+    if visible
+      @cards.join(" ")
+    else
+      (["*"]*@cards.length).join(" ")
+    end
+  end
+
+  def to_s
+    @name
+    #"#{@name} [#{show_cards}] => #{@score}"
   end
 end
