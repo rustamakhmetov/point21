62f9a72b77e9e6be2f11806dd930014e9d283e4a
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-11 22:53:00 +0500

    Заполнение, перемешивание колоды. Выдача карты

--- deck.rb
+++ deck.rb
@@ -14,14 +14,20 @@ class Deck
       (2..10).each do |nominal|
         @cards << Card.new(lear, Card::SIMPLE, nominal)
       end
-      ['J', 'Q', 'K'].each do |symbol|
+      %w(J Q K).each do |symbol|
         @cards << Card.new(lear, Card::PICTURE, 10, symbol)
       end
-      @cards << Card.new(lear, Card::ACE, 10, "A")
+      @cards << Card.new(lear, Card::ACE, 11, "A")
     end
+    shuffle!
   end
 
-  def shuffle
+  def shuffle!
     @cards.shuffle!
   end
+
+  def shift
+    @cards.shift
+  end
+
 end
