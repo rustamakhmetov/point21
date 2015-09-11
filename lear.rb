4398d778e25e23db3d2945a79ca3cecacdfc1d2a
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-11 22:50:53 +0500

    Константы как символы

--- lear.rb
+++ lear.rb
@@ -1,26 +1,10 @@
 # -*- encoding: utf-8 -*-
 
 class Lear
-  CLUBS = (Class.new do
-    def to_s
-      "+"
-    end
-  end).new
-  DIAMONDS = (Class.new do
-    def to_s
-      "<>"
-    end
-  end).new
-  HEARTS = (Class.new do
-    def to_s
-      "<3"
-    end
-  end).new
-  SPADES = (Class.new do
-    def to_s
-      "^"
-    end
-  end).new
+  CLUBS = :+
+  DIAMONDS = :<>
+  HEARTS = :"<3"
+  SPADES = :^
 
   def self.all
     [CLUBS, DIAMONDS, HEARTS, SPADES]
