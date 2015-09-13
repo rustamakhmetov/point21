68cb0f760b927cf9b1ba56310e8bdf763215ea75
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-13 14:44:34 +0500

    Создан класс Utils для общих методов

--- utils.rb
+++ utils.rb
@@ -1,2 +1,18 @@
 class Utils
+  def self.show_menu_with_select(menu, clear_screen=false)
+    menu_id = nil
+    loop do
+      system("clear") if clear_screen
+      puts "Выберите:"
+      menu.each {|k,v| puts "#{k}. #{v}"}
+      menu_id = gets.to_i
+      if menu.has_key?(menu_id)
+        break
+      else
+        puts "Неверный выбор"
+        sleep 2
+      end
+    end
+    menu_id
+  end
 end
