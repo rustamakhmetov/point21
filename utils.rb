70b4d8591c914b7c4c270d86d5bbc43d8b0c648e
Rustam Akhmetov <rustamakhmetov@yandex.ru>
2015-09-14 22:19:29 +0500

    Fix. Method has too many lines. [14/10]

--- utils.rb
+++ utils.rb
@@ -1,18 +1,27 @@
 class Utils
   def self.show_menu_with_select(menu, clear_screen = false)
     menu_id = nil
-    loop do
-      system('clear') if clear_screen
-      puts 'Выберите:'
-      menu.each { |k, v| puts "#{k}. #{v}" }
-      menu_id = gets.to_i
-      if menu.key?(menu_id)
-        break
-      else
-        puts 'Неверный выбор'
-        sleep 2
-      end
+    until menu_id
+      show_menu(menu, clear_screen)
+      menu_id = select_menu(menu)
     end
     menu_id
+  end
+
+  def self.show_menu(menu, clear_screen = false)
+    system('clear') if clear_screen
+    puts 'Выберите:'
+    menu.each { |k, v| puts "#{k}. #{v}" }
+  end
+
+  def self.select_menu(menu)
+    menu_id = gets.to_i
+    if menu.key?(menu_id)
+      menu_id
+    else
+      puts 'Неверный выбор'
+      sleep 2
+      false
+    end
   end
 end
